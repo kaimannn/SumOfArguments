42d42ee217bb8d204bd456b0b65f280f43c886ec
Pasqual Ferrari <57110836+kaimannn@users.noreply.github.com>
2023-01-21 12:29:09 +0100

    Update Program.cs

--- SumOfArguments.ConsoleApp/Program.cs
+++ SumOfArguments.ConsoleApp/Program.cs
@@ -9,14 +9,22 @@ try
         return;
     }
 
+    //
+
     Console.WriteLine($"The two entered arguments are '{args[0]}' and '{args[1]}'");
+
+    //
 
     var sumModel = args[0].Sum(args[1]);
     Console.WriteLine(sumModel.ToString());
 
+    //
+
     using var dbService = new DbService();
     _ = await dbService.SaveAsync(sumModel);
     Console.WriteLine($"The sum of the two arguments has been inserted into the DB");
+
+    //
 
     var sums = await dbService.GetAllSumsAsync();
     if (sums?.Count == 0)
@@ -24,6 +32,8 @@ try
         Console.WriteLine("The DB is empty");
         return;
     }
+
+    //
 
     Console.WriteLine($"The list of arguments in the DB is:");
     foreach (var sum in sums)
